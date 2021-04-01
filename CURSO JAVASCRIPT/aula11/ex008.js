35a878778bd954922f4e3aadd81f23ba01a0df28
uziel87 <80320234+uziel87@users.noreply.github.com>
2021-04-02 00:37:57 +0200

    estudos js avancando

---
+++ CURSO JAVASCRIPT/aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){
+    console.log('Voce ultrapassou a velocidade permitida. voce foi MULTADO')
+}
+
+console.log('Dirija sempre usando o cinto de segueança')
---
+++ CURSO JAVASCRIPT/aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'B*rasil'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log ('Voce e Brasileiro')
+}
+else {
+    console.log('Voce é estrangeiro')
+}
